92b8118f3a7d9f0309a0da371881e61e5840f96a
wolf-mtwo <wolf.mtwo@outlook.com>
2015-08-23 21:56:21 -0400

    Removed useless code

--- src/app/book/books.controller.js
+++ src/app/book/books.controller.js
@@ -6,36 +6,9 @@
     .controller('BookController', BookController);
 
   /** @ngInject */
-  function BookController($timeout, Books, toastr) {
+  function BookController($state, $timeout, Books, toastr) {
     var vm = this;
-
-    vm.awesomeThings = [];
-    vm.awesomeThings = Books.getAll();
-    vm.classAnimation = '';
-    vm.creationDate = 1440358344770;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      // vm.awesomeThings = webDevTec.getTec();
-      //
-      // angular.forEach(vm.awesomeThings, function(awesomeThing) {
-      //   awesomeThing.rank = Math.random();
-      // });
-    }
+    vm.params = $state.params;
+    toastr.info('Bienvenido!');
   }
 })();
